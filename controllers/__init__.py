148c9239d9085143293ca33fbf9b50cb34cbae96
GabrielCoelhoMendes <12402931@aluno.cotemig.com.br>
2026-07-01 09:49:08 -0300

    Delete controllers directory

--- controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dashboard_controller import dashboard_bp
-from .figurinhas_controller import figurinhas_bp
-from .api import api_v1_bp
-
-__all__ = ["dashboard_bp", "figurinhas_bp", "api_v1_bp"]
